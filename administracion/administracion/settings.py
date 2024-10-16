7699170dc9f7eacc379f3a00fc276e3b80758981
veronicahassen <veronicahassen@gmail.com>
2024-10-16 19:08:15 -0300

    Mysql connected

--- administracion/administracion/settings.py
+++ administracion/administracion/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'administracion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'seprice',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'default-character-set': 'utf8',
     }
 }
 
